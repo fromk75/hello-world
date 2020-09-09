79aea94732cb2ddac7efe423b88746013f3e28a9
fromk75 <fromk75@gmail.com>
2020-09-10 08:33:01 +0900

    second commit

--- HelloWorldMain.java
+++ HelloWorldMain.java
@@ -1,5 +1,6 @@
 public class HelloWorldMain<main> {
     public static void main(String[] args) {
-        System.out.print("HelloWorld!!");
+        System.out.print("HelloWorld !!");
+        System.out.print("Hello Java !!");
     }
 }
